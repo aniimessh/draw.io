6cd30e546aee06fecf6205d21023313bf6ec6a0e
aniimessh <animeshgupta2.002@gmail.com>
2024-03-12 15:07:16 +0530

    moving element feat developed, bug in line movement

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,44 @@ import rough from "roughjs";
 
 const generator = rough.generator();
 
-function createElement(x1, y1, x2, y2, elementType) {
+function createElement(id, x1, y1, x2, y2, type) {
   const roughElement =
-    elementType === "line"
+    type === "line"
       ? generator.line(x1, y1, x2, y2)
       : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-  return { x1, y1, x2, y2, roughElement };
+  return { id, x1, y1, x2, y2, type, roughElement };
+}
+
+function isWithinElement(x, y, element) {
+  const { type, x1, x2, y1, y2 } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return x >= minX && x <= maxX && y >= minY && y <= maxY;
+  } else if (type === "line") {
+    const a = { x: x1, y: y1 };
+    const b = { x: x2, y: y2 };
+    const c = { x, y };
+    const offSet = distance(a, b) - (distance(a, c) + distance(b, c));
+    return Math.abs(offSet) < 1;
+  }
+}
+
+const distance = (a, b) => {
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+};
+
+function getElementAtPosition(x, y, elements) {
+  return elements.find((element) => isWithinElement(x, y, element));
 }
 
 function App() {
   const [elements, setElement] = useState([]);
-  const [drawing, setDrawing] = useState(false);
-  const [elementType, setElementType] = useState("line");
+  const [action, setAction] = useState("none");
+  const [tool, setTool] = useState("line");
+  const [selectedElement, setSelectedElement] = useState(null);
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
@@ -26,33 +52,60 @@ function App() {
     elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
   }, [elements]);
 
-  const handleMouseDown = (event) => {
-    setDrawing(true);
-    const { clientX, clientY } = event;
-    const element = createElement(
-      clientX,
-      clientY,
-      clientX,
-      clientY,
-      elementType
-    );
-    setElement((prevState) => [...prevState, element]);
-  };
-  const handleMouseMove = (event) => {
-    if (!drawing) return;
-
-    const { clientX, clientY } = event;
-
-    const index = elements.length - 1;
-    const { x1, y1 } = elements[index];
-    const updatedElement = createElement(x1, y1, clientX, clientY, elementType);
+  const updateElement = (id, x1, y1, x2, y2, type) => {
+    const updatedElement = createElement(id, x1, y1, x2, y2, type);
 
     const copyelement = [...elements];
-    copyelement[index] = updatedElement;
+    copyelement[id] = updatedElement;
     setElement(copyelement);
   };
+
+  const handleMouseDown = (event) => {
+    const { clientX, clientY } = event;
+    if (tool === "selection") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      if (element) {
+        setSelectedElement(element);
+        setAction("moving");
+      }
+    } else {
+      const id = elements.length;
+      const element = createElement(
+        id,
+        clientX,
+        clientY,
+        clientX,
+        clientY,
+        tool
+      );
+      setElement((prevState) => [...prevState, element]);
+      setAction("drawing");
+    }
+  };
+
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    if (action === "drawing") {
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+      updateElement(index, x1, y1, clientX, clientY, tool);
+    } else if (action === "moving") {
+      const { id, x1, y1, x2, y2, type } = selectedElement;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      updateElement(
+        id,
+        clientX,
+        clientY,
+        clientX + width,
+        clientY + height,
+        type
+      );
+    }
+  };
   const handleMouseUp = () => {
-    setDrawing(false);
+    setAction("none");
+    setSelectedElement(null);
   };
 
   return (
@@ -60,16 +113,23 @@ function App() {
       <div style={{ position: "fixed" }}>
         <input
           type="radio"
+          id="selection"
+          checked={tool === "selection"}
+          onChange={() => setTool("selection")}
+        />
+        <label htmlFor="selection">Selection</label>
+        <input
+          type="radio"
           id="line"
-          checked={elementType === "line"}
-          onChange={() => setElementType("line")}
+          checked={tool === "line"}
+          onChange={() => setTool("line")}
         />
         <label htmlFor="line">Line</label>
         <input
           type="radio"
           id="rectangle"
-          checked={elementType === "rectangle"}
-          onChange={() => setElementType("rectangle")}
+          checked={tool === "rectangle"}
+          onChange={() => setTool("rectangle")}
         />
         <label htmlFor="rectangle">Rectangle</label>
       </div>
